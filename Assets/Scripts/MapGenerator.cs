f16bc2c004e742a20f1ce3c652c9dfa11da7fa38
VDSMath <matheusvdsdf@gmail.com>
2018-01-13 14:39:07 -0200

    UI

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    GameObject[,] Map;
+
+	// Use this for initialization
+	void Start () {
+        Map = new GameObject[2,2];
+        Debug.Log(Map);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
